7971c4ebd48ea12fbbb04d530c392914404a8161
koyo <koyo@koyo.kr>
2017-06-04 21:55:05 +0900

    ..

--- main.c
+++ main.c
@@ -7,32 +7,45 @@
 
 void aio_completion_handler(sigval_t sigval);
 
-void set_aiocb(struct aiocb *cbp, int fd) {
-    cbp->aio_fildes     = fd;
-    cbp->aio_lio_opcode = LIO_READ; /* for lio_listio */
-    cbp->aio_buf        = malloc(BUFSIZ);
-    cbp->aio_nbytes     = BUFSIZ;
-    cbp->aio_offset     = 0;
+struct aiocb *new_aiocb(int fd)
+{
+    struct aiocb *cbp = malloc(sizeof(struct aiocb));
+
+    cbp->aio_fildes = fd;
+    cbp->aio_buf    = malloc(BUFSIZ);
+    cbp->aio_nbytes = BUFSIZ;
+    cbp->aio_offset = 0;
+    cbp->aio_lio_opcode = LIO_READ;
     cbp->aio_sigevent.sigev_notify            = SIGEV_THREAD;
     cbp->aio_sigevent.sigev_notify_function   = aio_completion_handler;
     cbp->aio_sigevent.sigev_notify_attributes = NULL;
     cbp->aio_sigevent.sigev_value.sival_ptr   = cbp;
+
+    return cbp;
 }
 
-void unix_error(const char *s) {
+void delete_aiocb(struct aiocb *cbp)
+{
+    free((void *)cbp->aio_buf);
+    free(cbp);
+}
+
+void unix_error(const char *s)
+{
     perror(s);
     exit(EXIT_FAILURE);
 }
 
 int g_fd;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     if (argc != 2) {
-        printf("usage: %s <port>\n", argv[0]);
+        puts("usage: aio_echo_server <port>");
         return 1;
     }
 
-    struct sockaddr_in ssin = {
+    struct sockaddr_in sa = {
         .sin_family      = AF_INET,
         .sin_port        = htons(atoi(argv[1])),
         .sin_addr.s_addr = htonl(INADDR_ANY)
@@ -42,17 +55,16 @@ int main(int argc, char *argv[]) {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd == -1)
         unix_error("socket");
-    if (bind(fd, (struct sockaddr *)&ssin, sizeof(struct sockaddr)) == -1)
+    if (bind(fd, (struct sockaddr *)&sa, sizeof(struct sockaddr)) == -1)
         unix_error("bind");
     if (listen(fd, 5) == -1)
         unix_error("listen");
 
     /* set */
-    struct aiocb cb[2] = {};
-    struct aiocb *cblist[2] = { &cb[0], &cb[1] };
-
-    set_aiocb(cblist[0], STDIN_FILENO);
-    set_aiocb(cblist[1], fd);
+    struct aiocb *cblist[2] = {
+        new_aiocb(STDIN_FILENO),
+        new_aiocb(fd)
+    };
 
     g_fd = fd;
 
@@ -63,64 +75,48 @@ int main(int argc, char *argv[]) {
     pause();
 }
 
-void aio_completion_handler(sigval_t sigval) {
+void aio_completion_handler(sigval_t sigval)
+{
     struct aiocb *cbp = sigval.sival_ptr;
-    if (aio_error(cbp) == -1)
-        unix_error("aio_handler");
-
-    char *buf = (char *)cbp->aio_buf;
+    int fd = cbp->aio_fildes;
+    char *buf = (void *)cbp->aio_buf;
     int buflen = aio_return(cbp);
 
-    if (cbp->aio_fildes == STDIN_FILENO) {
-        /* exit */
+    if (fd == STDIN_FILENO) { /* stdin */
         buf[buflen] = '\0';
         if (!strcmp(buf, "exit\n"))
             exit(EXIT_SUCCESS);
-
-        /* aio_read */
-        if (aio_read(cbp) == -1)
-            unix_error("aio_read");
     }
-    else if (cbp->aio_fildes == g_fd) {
-        /* accept */
-        struct sockaddr_in csin;
-        socklen_t socklen = sizeof(struct sockaddr);
+    else if (fd == g_fd) { /* new client */
+        struct sockaddr_in sa;
+        socklen_t slen;
 
-        int cfd = accept(g_fd, (struct sockaddr *)&csin, &socklen);
+        int cfd = accept(fd, (struct sockaddr *)&sa, &slen);
         if (cfd == -1)
             unix_error("accept");
-        printf("\x1b[0;32m[*] aceept\x1b[0m\n");
+        puts("\x1b[0;32m[*] aceept\x1b[0m");
 
-        /* new client */
-        struct aiocb *ccbp = malloc(sizeof(struct aiocb));
-        set_aiocb(ccbp, cfd);
-
-        /* aio_read */
-        if (aio_read(cbp)  == -1 ||
-            aio_read(ccbp) == -1)
+        struct aiocb *ccbp = new_aiocb(cfd);
+        if (aio_read(ccbp) == -1)
             unix_error("aio_read");
     }
     else {
-        int cfd = cbp->aio_fildes;
-        if (buflen <= 0) {
-            /* close */
-            close(cfd);
-            printf("\x1b[0;31m[*] close\x1b[0m\n");
+        if (buflen <= 0) { /* delete client */
+            close(fd);
+            puts("\x1b[0;31m[*] close\x1b[0m");
 
-            /* delete client */
-            free(buf);
-            free(cbp);
+            delete_aiocb(cbp);
+            return;
         }
-        else {
-            /* write */
+        else { /* echo */
             buf[buflen] = '\0';
-            if (write(cfd, buf, buflen) != buflen)
+            if (write(fd, buf, buflen) != buflen)
                 unix_error("write");
-            printf("%s", buf);
-
-            /* aio_read */
-            if (aio_read(cbp) == -1)
-                unix_error("aio_read");
+            puts(buf);
         }
     }
+
+    /* aio_read */
+    if (aio_read(cbp) == -1)
+        unix_error("aio_read");
 }
